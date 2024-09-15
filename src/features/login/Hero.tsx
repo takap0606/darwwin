58d1d4ea9761f8f00812d7563a648fa6f74b5368
takap0606 <takap0606@gmail.com>
2024-09-15 21:38:14 +0900

    チャットを追加

--- src/features/login/Hero.tsx
+++ src/features/login/Hero.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Grid, Typography, styled } from '@mui/material';
+import Script from 'next/script';
 
 const TypographyH2 = styled(Typography)(
   ({ theme }) => `
@@ -65,27 +66,6 @@ const BybitLogo = styled(Box)(
 `,
 );
 
-const NftImageAvatar = styled(Box)(
-  ({ theme }) => `
-    width: ${theme.spacing(8)};
-    height: ${theme.spacing(8)};
-    border-radius: ${theme.general.borderRadius};
-    background-color: #fff;
-    flex-shrink: 0;
-    overflow: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: ${theme.spacing(2)};
-
-    img {
-      width: 100%;
-      height: 100%;
-      display: block;
-    }
-`,
-);
-
 const HeroLogoImage = styled(Box)(
   () => `
     img {
@@ -97,62 +77,75 @@ const HeroLogoImage = styled(Box)(
 
 function Hero() {
   return (
-    <Container maxWidth="lg">
-      <Grid
-        spacing={{ xs: 6, md: 10 }}
-        justifyContent="center"
-        alignItems="center"
-        container
-      >
-        <Grid item md={6} pr={{ xs: 0, md: 3 }}>
-          <HeroLogoImage mb={2} pr={8} maxWidth="500px" display="block">
-            <img src="/static/images/hero/hero_title.png" alt="" />
-          </HeroLogoImage>
-          <TypographyH2
-            sx={{
-              lineHeight: 1.5,
-              pb: 4,
-            }}
-            variant="h4"
-            color="text.secondary"
-            fontWeight="normal"
-          >
-            Welcome to DARWWIN, your gateway to the future of NFT staking and
-            digital asset growth.
-            <br /> At DARWWIN, we empower you to unlock the full potential of
-            your NFTs by staking and growing your digital assets with ease. Our
-            platform is designed to take advantage of the booming cryptocurrency
-            market, helping you maximize your portfolio during the bull run.
-            Whether you’re a beginner or an expert, our intuitive interface and
-            robust security make it simple to manage your assets and track your
-            progress in real time.
-            <br /> Join DARWWIN today and experience a secure, innovative way to
-            grow your NFT portfolio. With us, you’re not just investing in
-            NFTs—you’re stepping into the next evolution of digital finance.
-          </TypographyH2>
-          <Typography variant="h2">Partnership</Typography>
-          <ListItemWrapper>
-            <BybitLogo>
-              <img src="/static/images/hero/roadmap1.svg" alt="" />
-            </BybitLogo>
-          </ListItemWrapper>
-        </Grid>
-        <Grid item md={6}>
-          <BoxContent>
-            <Box>
-              <ImgWrapper>
-                <img src="/static/images/hero/frontend.png" alt="" />
-              </ImgWrapper>
-            </Box>
-            <BoxAccent
+    <>
+      <Container maxWidth="lg">
+        <Grid
+          spacing={{ xs: 6, md: 10 }}
+          justifyContent="center"
+          alignItems="center"
+          container
+        >
+          <Grid item md={6} pr={{ xs: 0, md: 3 }}>
+            <HeroLogoImage mb={2} pr={8} maxWidth="500px" display="block">
+              <img src="/static/images/hero/hero_title.png" alt="" />
+            </HeroLogoImage>
+            <TypographyH2
               sx={{
-                display: { xs: 'none', md: 'block' },
+                lineHeight: 1.5,
+                pb: 4,
               }}
-            />
-          </BoxContent>
+              variant="h4"
+              color="text.secondary"
+              fontWeight="normal"
+            >
+              Welcome to DARWWIN, your gateway to the future of NFT staking and
+              digital asset growth.
+              <br /> At DARWWIN, we empower you to unlock the full potential of
+              your NFTs by staking and growing your digital assets with ease.
+              Our platform is designed to take advantage of the booming
+              cryptocurrency market, helping you maximize your portfolio during
+              the bull run. Whether you’re a beginner or an expert, our
+              intuitive interface and robust security make it simple to manage
+              your assets and track your progress in real time.
+              <br /> Join DARWWIN today and experience a secure, innovative way
+              to grow your NFT portfolio. With us, you’re not just investing in
+              NFTs—you’re stepping into the next evolution of digital finance.
+            </TypographyH2>
+            <Typography variant="h2">Partnership</Typography>
+            <ListItemWrapper>
+              <BybitLogo>
+                <img src="/static/images/hero/roadmap1.svg" alt="" />
+              </BybitLogo>
+            </ListItemWrapper>
+          </Grid>
+          <Grid item md={6}>
+            <BoxContent>
+              <Box>
+                <ImgWrapper>
+                  <img src="/static/images/hero/frontend.png" alt="" />
+                </ImgWrapper>
+              </Box>
+              <BoxAccent
+                sx={{
+                  display: { xs: 'none', md: 'block' },
+                }}
+              />
+            </BoxContent>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Script
+        id="crisp-chat-int"
+        type="text/javascript"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.$crisp=[];window.CRISP_WEBSITE_ID="c254ca73-acf4-4ae4-aea6-2dcec718649a";
+  (function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";
+  s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+        }}
+      />
+    </>
   );
 }
 
