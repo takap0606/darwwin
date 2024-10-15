fb13b558385133ff63e61728035513dad7d63c6f
takap0606 <takap0606@gmail.com>
2024-10-15 17:51:09 +0900

    allowedAddresses修正

--- src/features/admin/utils/allowedAddresses.ts
+++ src/features/admin/utils/allowedAddresses.ts
@@ -2,6 +2,7 @@ const allowedAddresses: string[] = [
   '0xd8ed53f3cb308beeda8d56cec661586428cfd797',
   '0x1a9429fc11d9de7e8a933ea78654686a7324ab4e',
   '0xe1641b7b089a409ce49242bf716a9b500c810a6a', // 竹村さん
+  '0x5e4ed1fd9be9d77e60e2e5d39eeca9554bfd7dd1', // サポート
 ];
 
 export default allowedAddresses;
